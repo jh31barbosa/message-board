e78e56b59ca7feaad8aada132e853dde066a9524
jh31barbosa <vazavult@gmail.com>
2023-04-23 16:32:28 -0300

    add ".env"

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e1vh@z3ikktwvrp4cb()vl!@tn-c5p(2+b@#t!h+@gzugl4qm8'
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
